0aec4b8e2453d62c6904d83002a1551912ee39c0
Defelo <elodef42@gmail.com>
2023-04-11 15:36:45 +0200

    Add environments as nix packages

--- flake.nix
+++ flake.nix
@@ -7,22 +7,47 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
     packages = import ./packages {inherit pkgs;};
+    envs =
+      builtins.mapAttrs (k: v: {
+        inherit (v) name version;
+        compile_script =
+          if builtins.isNull v.compile_script
+          then null
+          else pkgs.writeShellScript "${k}-compile.sh" v.compile_script;
+        run_script = pkgs.writeShellScript "${k}-run.sh" v.run_script;
+      })
+      packages;
     environments = pkgs.writeText "environments.json" (builtins.toJSON {
       nsjail_path = "${pkgs.nsjail}/bin/nsjail";
       time_path = "${pkgs.time}/bin/time";
-      environments =
-        builtins.mapAttrs (k: v: {
-          inherit (v) name version;
-          compile_script =
-            if builtins.isNull v.compile_script
-            then null
-            else pkgs.writeShellScript "${k}-compile.sh" v.compile_script;
-          run_script = pkgs.writeShellScript "${k}-run.sh" v.run_script;
-        })
-        packages;
+      environments = envs;
     });
   in {
     packages.${system} = rec {
+      packages = builtins.mapAttrs (k: {
+        name,
+        version,
+        compile_script,
+        run_script,
+      }:
+        pkgs.stdenv.mkDerivation {
+          inherit name version;
+          unpackPhase = "true";
+          installPhase = let
+            sandbox = file:
+              pkgs.writeShellScript "run-in-sandbox.sh"
+              ''${pkgs.nsjail}/bin/nsjail -q --cwd /box -B $PWD/box:/box -B $PWD/program:/out -E MAIN -R /nix/store -R $PWD/program:/program -T /tmp -- ${file} "$@"'';
+          in ''
+            mkdir -p $out/bin
+            ${
+              if compile_script != null
+              then "ln -s ${sandbox compile_script} $out/bin/${k}-compile.sh"
+              else ""
+            }
+            ln -s ${sandbox run_script} $out/bin/${k}-run.sh
+          '';
+        })
+      envs;
       rust = pkgs.rustPlatform.buildRustPackage {
         name = "sandkasten";
         src = pkgs.stdenv.mkDerivation {
