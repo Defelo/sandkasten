e1a5354b0e7328738c75034dba3f0a48ae03cc00
Defelo <elodef42@gmail.com>
2023-04-14 11:40:23 +0200

    Refactor tests

--- tests/api.rs
+++ tests/api.rs
@@ -1,15 +1,12 @@
 use std::collections::HashMap;
 
-use common::build_and_run;
 use indoc::formatdoc;
 use sandkasten::schemas::{
     environments::Environment,
-    programs::{
-        BuildRequest, BuildRunRequest, BuildRunResult, File, LimitsOpt, RunRequest, RunResult,
-    },
+    programs::{BuildRequest, BuildRunRequest, BuildRunResult, File, RunRequest, RunResult},
 };
 
-use crate::common::{url, BuildError};
+use crate::common::{build_and_run, url, BuildError};
 
 mod common;
 
@@ -195,95 +192,4 @@ async fn test_build_cached() {
         .unwrap();
     assert_eq!(response.status, 0);
     assert_eq!(response.stdout, "test\n");
-}
-
-#[tokio::test]
-#[ignore]
-async fn test_no_internet() {
-    let response = build_and_run(&BuildRunRequest {
-        build: BuildRequest {
-            environment: "python".into(),
-            files: vec![File {
-                name: "test.py".into(),
-                content: formatdoc! {r#"
-                    from http.client import *
-                    c=HTTPConnection("1.1.1.1")
-                    c.request("GET", "http://1.1.1.1")
-                "#},
-            }],
-            compile_limits: Default::default(),
-        },
-        run: Default::default(),
-    })
-    .await
-    .unwrap();
-    assert_eq!(response.run.status, 1);
-    assert_eq!(
-        response.run.stderr.trim().lines().last().unwrap(),
-        "OSError: [Errno 101] Network is unreachable"
-    );
-}
-
-#[tokio::test]
-#[ignore]
-async fn test_forkbomb() {
-    let response = build_and_run(&BuildRunRequest {
-        build: BuildRequest {
-            environment: "python".into(),
-            files: vec![File {
-                name: "test.py".into(),
-                content: formatdoc! {"
-                    import os
-                    while True:
-                        os.fork()
-                "},
-            }],
-            compile_limits: Default::default(),
-        },
-        run: Default::default(),
-    })
-    .await
-    .unwrap();
-    assert_eq!(response.run.status, 1);
-    assert_eq!(
-        response.run.stderr.trim().lines().last().unwrap(),
-        "BlockingIOError: [Errno 11] Resource temporarily unavailable"
-    );
-    assert!(response.run.resource_usage.time < 1000);
-}
-
-#[tokio::test]
-#[ignore]
-async fn test_stdoutbomb() {
-    let response = build_and_run(&BuildRunRequest {
-        build: BuildRequest {
-            environment: "rust".into(),
-            files: vec![File {
-                name: "test.rs".into(),
-                content: formatdoc! {r#"
-                    fn main() {{
-                        loop {{
-                            println!("spam");
-                            eprintln!("maps");
-                        }}
-                    }}
-                "#},
-            }],
-            compile_limits: Default::default(),
-        },
-        run: RunRequest {
-            run_limits: LimitsOpt {
-                time: Some(1),
-                stdout_max_size: Some(2048),
-                stderr_max_size: Some(1024),
-                ..Default::default()
-            },
-            ..Default::default()
-        },
-    })
-    .await
-    .unwrap();
-    assert_eq!(response.run.status, 137);
-    assert_eq!(response.run.stdout.len(), 2048);
-    assert_eq!(response.run.stderr.len(), 1024);
 }
---
+++ tests/attacks.rs
@@ -0,0 +1,97 @@
+use indoc::formatdoc;
+use sandkasten::schemas::programs::{BuildRequest, BuildRunRequest, File, LimitsOpt, RunRequest};
+
+use crate::common::build_and_run;
+
+mod common;
+
+#[tokio::test]
+#[ignore]
+async fn test_no_internet() {
+    let response = build_and_run(&BuildRunRequest {
+        build: BuildRequest {
+            environment: "python".into(),
+            files: vec![File {
+                name: "test.py".into(),
+                content: formatdoc! {r#"
+                    from http.client import *
+                    c=HTTPConnection("1.1.1.1")
+                    c.request("GET", "http://1.1.1.1")
+                "#},
+            }],
+            compile_limits: Default::default(),
+        },
+        run: Default::default(),
+    })
+    .await
+    .unwrap();
+    assert_eq!(response.run.status, 1);
+    assert_eq!(
+        response.run.stderr.trim().lines().last().unwrap(),
+        "OSError: [Errno 101] Network is unreachable"
+    );
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_forkbomb() {
+    let response = build_and_run(&BuildRunRequest {
+        build: BuildRequest {
+            environment: "python".into(),
+            files: vec![File {
+                name: "test.py".into(),
+                content: formatdoc! {"
+                    import os
+                    while True:
+                        os.fork()
+                "},
+            }],
+            compile_limits: Default::default(),
+        },
+        run: Default::default(),
+    })
+    .await
+    .unwrap();
+    assert_eq!(response.run.status, 1);
+    assert_eq!(
+        response.run.stderr.trim().lines().last().unwrap(),
+        "BlockingIOError: [Errno 11] Resource temporarily unavailable"
+    );
+    assert!(response.run.resource_usage.time < 1000);
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_stdoutbomb() {
+    let response = build_and_run(&BuildRunRequest {
+        build: BuildRequest {
+            environment: "rust".into(),
+            files: vec![File {
+                name: "test.rs".into(),
+                content: formatdoc! {r#"
+                    fn main() {{
+                        loop {{
+                            println!("spam");
+                            eprintln!("maps");
+                        }}
+                    }}
+                "#},
+            }],
+            compile_limits: Default::default(),
+        },
+        run: RunRequest {
+            run_limits: LimitsOpt {
+                time: Some(1),
+                stdout_max_size: Some(2048),
+                stderr_max_size: Some(1024),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+    })
+    .await
+    .unwrap();
+    assert_eq!(response.run.status, 137);
+    assert_eq!(response.run.stdout.len(), 2048);
+    assert_eq!(response.run.stderr.len(), 1024);
+}
