161ed4c3c9b1294467dd14ee00088e0cdbfa21d6
Defelo <elodef42@gmail.com>
2023-04-18 17:33:23 +0200

    Improve c test

--- nix/packages/c.nix
+++ nix/packages/c.nix
@@ -7,10 +7,38 @@
     {
       name = "test.c";
       content = ''
-        #include <stdio.h>
-        int main() {
-          printf("OK");
+        #define  _GNU_SOURCE
+        #include <stdlib.h>
+        #include <string.h>
+        #include "foo.c"
+
+        int main(int argc, char** argv) {
+          size_t bufsize = 5;
+          char* buffer = (char *)malloc(bufsize * sizeof(char));
+          size_t characters = getline(&buffer,&bufsize,stdin);
+          if (strcmp(buffer, "stdin")) return 1;
+
+          if (argc-1 != 3) return 2;
+          if (strcmp(argv[1], "foo")) return 3;
+          if (strcmp(argv[2], "bar")) return 4;
+          if (strcmp(argv[3], "baz")) return 5;
+
+          FILE* fptr = fopen("test.txt", "r");
+          char content[12];
+          fgets(content, 12, fptr);
+          if (strcmp(content, "hello world")) return 6;
+
+          bar();
           return 0;
+        }
+      '';
+    }
+    {
+      name = "foo.c";
+      content = ''
+        #include <stdio.h>
+        void bar() {
+          printf("OK");
         }
       '';
     }
