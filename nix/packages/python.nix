af712db8f0e04a362f449b1ea521eb9f9567bfdb
Defelo <elodef42@gmail.com>
2023-04-18 17:53:06 +0200

    Improve python test

--- nix/packages/python.nix
+++ nix/packages/python.nix
@@ -7,7 +7,18 @@
     {
       name = "test.py";
       content = ''
+        import sys
         import foo
+
+        if input() != "stdin": exit(1)
+
+        if len(sys.argv)-1 != 3: exit(2)
+        if sys.argv[1] != "foo": exit(3)
+        if sys.argv[2] != "bar": exit(4)
+        if sys.argv[3] != "baz": exit(5)
+
+        if open("test.txt").read() != "hello world": exit(6)
+
         foo.bar()
       '';
     }
