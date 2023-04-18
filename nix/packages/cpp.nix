424b10df55b730385198dcacd9e348412b38c7bb
Defelo <elodef42@gmail.com>
2023-04-18 17:49:36 +0200

    Improve cpp test

--- nix/packages/cpp.nix
+++ nix/packages/cpp.nix
@@ -7,8 +7,33 @@
     {
       name = "test.cpp";
       content = ''
+        #include "foo.cpp"
+
+        int main(int argc, char** argv) {
+          std::string s;
+          std::cin >> s;
+          if (s != "stdin") return 1;
+
+          if (argc-1 != 3) return 2;
+          if (strcmp(argv[1], "foo")) return 3;
+          if (strcmp(argv[2], "bar")) return 4;
+          if (strcmp(argv[3], "baz")) return 5;
+
+          std::ifstream file;
+          file.open("test.txt");
+          getline(file, s);
+          if (s != "hello world") return 6;
+
+          bar();
+          return 0;
+        }
+      '';
+    }
+    {
+      name = "foo.cpp";
+      content = ''
         #include <bits/stdc++.h>
-        int main() {
+        void bar() {
           std::cout << "OK";
         }
       '';
