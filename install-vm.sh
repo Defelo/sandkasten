63df4db98662c71127e7068373e3613c46e44e1d
Defelo <elodef42@gmail.com>
2023-06-07 11:43:18 +0200

    Add flake template for vm

--- install-vm.sh
+++ install-vm.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-REPO=${REPO:-https://github.com/Defelo/sandkasten.git}
+FLAKE=${FLAKE:-github:Defelo/sandkasten#vm}
 DISK=${1:-/dev/sda}
 ROOT=${2:-${DISK}1}
 
@@ -17,8 +17,14 @@ nix profile install nixpkgs#git
 
 echo -ne 'o\nn\n\n\n\n\na\nw\n' | fdisk $DISK
 mkfs.ext4 $ROOT
+
 mount $ROOT /mnt
 mkdir /mnt/root
-git clone "$REPO" /mnt/root/sandkasten
+nix flake new --template "$FLAKE" /mnt/root/sandkasten
+
+nixos-generate-config --root /mnt --show-hardware-config > /mnt/root/sandkasten/hardware-configuration.nix
+sed -i -E "s#boot.loader.grub.device = \"/dev/sda\";#boot.loader.grub.device = \"$DISK\";#" /mnt/root/sandkasten/configuration.nix
+
 nixos-install --flake /mnt/root/sandkasten#sandkasten --no-channel-copy --no-root-password
+
 reboot
