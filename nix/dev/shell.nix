5adc4071cd78fb03b8896945e61a725c19473828
Defelo <elodef42@gmail.com>
2023-06-18 14:57:13 +0200

    Suppress request logs in integration tests

--- nix/dev/shell.nix
+++ nix/dev/shell.nix
@@ -69,7 +69,7 @@
     rm -rf programs jobs
     echo 'save ""' | ${pkgs.redis}/bin/redis-server - &
     redis_pid=$!
-    cargo llvm-cov run --lcov --output-path lcov-server.info --release --locked -F test_api &
+    RUST_LOG=info,poem::middleware::tracing_mw=off cargo llvm-cov run --lcov --output-path lcov-server.info --release --locked -F test_api &
     pid=$!
     while ! ${pkgs.curl}/bin/curl -so/dev/null localhost:8000; do
       sleep 1
