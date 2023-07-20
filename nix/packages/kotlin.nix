789945db4194cf1bbafd8a44561d2ea718747fb0
Defelo <elodef42@gmail.com>
2023-07-20 13:06:01 +0200

    Fix kotlin compile script

--- nix/packages/kotlin.nix
+++ nix/packages/kotlin.nix
@@ -5,7 +5,7 @@
     inherit (pkgs.kotlin.meta) description longDescription homepage;
   };
   default_main_file_name = "code.kt";
-  compile_script = ''PATH=${pkgs.coreutils}/bin ${pkgs.kotlin}/bin/kotlinc -d /program/program.jar "$@"'';
+  compile_script = ''PATH=${pkgs.coreutils}/bin ${pkgs.kotlin}/bin/kotlinc -include-runtime -d /program/program.jar "$@"'';
   run_script = ''shift; PATH=${pkgs.coreutils}/bin ${pkgs.kotlin}/bin/kotlin /program/program.jar "$@"'';
   test.main_file.content = ''
     import foo.bar;
